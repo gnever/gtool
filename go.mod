c81a8c89e0510af6deaedd16988ba9613c911c41
佚名 <994516762@qq.com>
2019-12-03 14:11:40 +0800

    add daemon example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gnever/gtool
 
 go 1.13
+
+require github.com/gogf/gf v1.9.10
---
+++ godaemon/example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+
+	_ "github.com/gnever/gtool/godaemon"
+	"github.com/gogf/gf/os/glog"
+)
+
+func main() {
+
+	l := glog.New().Line(true)
+	l.SetPath("/tmp/")
+	l.SetStdoutPrint(true)
+
+	l.Debugf("start  %d", time.Now().Unix())
+	for i := 0; i < 100; i++ {
+		l.Debugf("do  %d", time.Now().Unix())
+		time.Sleep(time.Second)
+	}
+	l.Debugf("end  %d", time.Now().Unix())
+
+}
